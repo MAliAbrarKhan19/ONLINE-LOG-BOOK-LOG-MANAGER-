88aaf049845239f0c9d8a00eedbad25efce57b69
ananyasaha2468 <69783876+ananyasaha2468@users.noreply.github.com>
2020-09-15 21:57:21 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Blood Donator Finder</title>
+  </head>
+  <body>
+
+    <div class="container-fluid">
+    <!-- =========================================
+                          Nav bar
+        ============================================ -->
+
+          <div class="row bg-secondary">
+            <div class="col-md-12">
+            
+                <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                  <a class="navbar-brand" href="./index.php">Log Manager</a>
+
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                      <ul class="navbar-nav mr-2 mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                          <a class="nav-link" href="./index.php">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                          <a class="nav-link" href="#">List </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Staff Register</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="./admin.php">Admin</a>
+                        </li>
+                        
+                      </ul>
+                   
+                    </div>
+                </nav>
+
+
+          </div>  
+          </div>  
+
+    <!-- =========================================
+                          Nav bar
+        ============================================ -->
+
+
+
+
+        <!--------------------------------- Header Ends----------------------- -->
+
