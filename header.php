8143eecea4338a1c1c2687a94a4fe88506fc649b
ananyasaha2468 <69783876+ananyasaha2468@users.noreply.github.com>
2020-09-16 11:54:23 +0600

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="en">
-  <head>
+    <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="./css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     <title>Blood Donator Finder</title>
-  </head>
+    </head>
   <body>
 
     <div class="container-fluid">
@@ -51,12 +51,10 @@
           </div>  
           </div>  
 
-    <!-- =========================================
-                          Nav bar
-        ============================================ -->
+    <!-- ========================================= Nav bar============================================ -->
 
 
 
 
-        <!--------------------------------- Header Ends----------------------- -->
+        <!--Header -->
 
